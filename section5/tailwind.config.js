3e7b742bded0ad8388c070278681f9b7d090546c
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-29 12:04:09 +0900

    初期設定とcolorの追加

---
+++ section5/tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'sky': colors.sky,
+        fresh: {
+          100: '#BBD3FB',
+          200: '#9FBFF5',
+          300: '#81A9EE',
+          400: '#6A96E1',
+          500: '#5985D0',
+          600: '#3B69B7',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+}
